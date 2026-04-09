61c66f7feb324b0009caebbb6678b02c30b3cf80
SyedFaris04 <syedamirfaris@gmail.com>
2026-04-09 23:21:39 +0800

    Delete trading_system/app directory

--- trading_system/app/components/__init__.py
+++
@@ -1,8 +0,0 @@
-from components import (
-    page_dashboard, page_stock_analysis,
-    page_model_comparison, page_backtesting,
-    page_sentiment, page_education,
-    page_predictions, page_portfolio,
-    page_export, page_game, page_auth,
-    page_simulator, page_alerts,
-)
